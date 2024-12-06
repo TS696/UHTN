308b187d34a6dff54580189721c0f501e6335681
TS696 <takuhanccnxx@gmail.com>
2024-12-06 22:43:45 +0900

    Refactor

--- Assets/UHTN/Runtime/Core/Plan.cs
+++ Assets/UHTN/Runtime/Core/Plan.cs
@@ -7,27 +7,25 @@ namespace UHTN
     {
         public int TargetTaskIndex { get; }
 
-        private readonly int[] _tasks;
-        public int[] Tasks => _tasks;
+        public int[] Tasks { get; }
 
-        private readonly int[] _methodTraversalRecord;
-        public int[] MethodTraversalRecord => _methodTraversalRecord;
+        public int[] MethodTraversalRecord { get; }
 
         public readonly List<(int taskIndex, Plan plan)> PartialPlans = new();
 
-        public bool IsEmpty => _tasks.Length == 0;
+        public bool IsEmpty => Tasks.Length == 0;
 
         public Plan(int[] tasks, int targetTaskIndex, int[] methodTraversalRecord)
         {
-            _tasks = tasks;
+            Tasks = tasks;
             TargetTaskIndex = targetTaskIndex;
-            _methodTraversalRecord = methodTraversalRecord;
+            MethodTraversalRecord = methodTraversalRecord;
         }
 
         public override string ToString()
         {
-            var tasks = string.Join(",", _tasks.Select(x => x.ToString()));
-            var mtr = string.Join(",", _methodTraversalRecord.Select(x => x.ToString()));
+            var tasks = string.Join(",", Tasks.Select(x => x.ToString()));
+            var mtr = string.Join(",", MethodTraversalRecord.Select(x => x.ToString()));
             return $"Tasks: {tasks}\nMTR: {mtr}";
         }
     }
